983836d4b954cf7010ac7d3c4b6afd38e302748d
Amina-S <as2642@cornell.edu>
2020-10-07 17:10:11 -0400

    Create data_collect.py

---
+++ data_collect.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+!conda install --yes --prefix {sys.prefix} requests
+!conda install --yes --prefix {sys.prefix} bs4
+import requests
+import bs4
